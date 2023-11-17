a8e7bcf212268fd528c550eec2efd5c2d264a61a
Ruiyot Abby <ruiyotabby@gmail.com>
2023-11-17 22:49:18 +0300

    Add lint

--- bmiCalculator.ts
+++ bmiCalculator.ts
@@ -1,42 +1,41 @@
-export const calculateBmi =  (height: number, weight: number): string => {
-  const bmi: number = weight/((height/100)**2)
-  switch(true) {
-    case bmi < 18.5:
-      return 'Underweight'
-    case 25 < bmi && bmi > 18.5:
-      return 'normal weight'
-    case bmi > 25:
-      return 'overweight'
-    default:
-      return 'some error occured'
-  }
-}
+export const calculateBmi = (height: number, weight: number): string => {
+	const bmi: number = weight / (height / 100) ** 2;
+	switch (true) {
+		case bmi < 18.5:
+			return 'Underweight';
+		case 25 < bmi && bmi > 18.5:
+			return 'normal weight';
+		case bmi > 25:
+			return 'overweight';
+		default:
+			return 'some error occured';
+	}
+};
 
 interface bmiValues {
-  value1: number,
-  value2: number
+	value1: number;
+	value2: number;
 }
 
 const parseArguments = (args: string[]): bmiValues => {
-  if (args.length < 4) throw new Error("Not enough arguments");
-  if (args.length > 4) throw new Error("Too many arguments");
-  if (isNaN(Number(args[2])) && isNaN(Number(args[3]))) {
-    throw new Error("provided values were not numbers");
-  }
-  return {
-    value1: Number(args[2]),
-    value2: Number(args[3])
-  }
-}
+	if (args.length < 4) throw new Error('Not enough arguments');
+	if (args.length > 4) throw new Error('Too many arguments');
+	if (isNaN(Number(args[2])) && isNaN(Number(args[3]))) {
+		throw new Error('provided values were not numbers');
+	}
+	return {
+		value1: Number(args[2]),
+		value2: Number(args[3]),
+	};
+};
 
 try {
-  const { value1, value2 } = parseArguments(process.argv)
-  console.log(calculateBmi(value1, value2));
+	const { value1, value2 } = parseArguments(process.argv);
+	console.log(calculateBmi(value1, value2));
 } catch (error: unknown) {
-  let errorMessage: string = 'some error happened. ';
-  if (error instanceof Error) {
-    errorMessage += 'Error: ' + error.message
-  }
-  console.log(errorMessage);
-
+	let errorMessage: string = 'some error happened. ';
+	if (error instanceof Error) {
+		errorMessage += 'Error: ' + error.message;
+	}
+	console.log(errorMessage);
 }
--- exerciseCalculator.ts
+++ exerciseCalculator.ts
@@ -1,57 +1,72 @@
 interface Result {
-  periodLength: number;
-  trainingDays: number;
-  success: boolean;
-  rating: number;
-  ratingDescription: string;
-  target: number;
-  average: number;
+	periodLength: number;
+	trainingDays: number;
+	success: boolean;
+	rating: number;
+	ratingDescription: string;
+	target: number;
+	average: number;
 }
 
-interface exerciseValues {
-  value1: number,
-  value2: number[]
+export interface exerciseValues {
+	target: number;
+	dailyExercises: number[];
 }
 
 const parseArguments = (args: string[]): exerciseValues => {
-  if (args.length <= 3) throw new Error('Not enough arguments')
+	if (args.length <= 3) throw new Error('Not enough arguments');
 
-  const values: string[] = args.slice(3)
+	const values: string[] = args.slice(3);
 
-  if (isNaN(Number(args[2])) || !values.every(value => !isNaN(Number(value)))) {
-    throw new Error("provided values were not numbers");
-  } else {
-    return {
-      value1: Number(args[2]),
-      value2: args.splice(3).map(i => Number(i))
-    }
-  }
-}
+	if (
+		isNaN(Number(args[2])) ||
+		!values.every((value) => !isNaN(Number(value)))
+	) {
+		throw new Error('provided values were not numbers');
+	} else {
+		return {
+			target: Number(args[2]),
+			dailyExercises: args.splice(3).map((i) => Number(i)),
+		};
+	}
+};
 
-export const calculateExercise = (dailyExercises: number[], target: number): Result => {
-  let result : Result = {
-    target,
-    periodLength: dailyExercises.length,
-    trainingDays: dailyExercises.filter(dailyExercise => dailyExercise !== 0).length,
-    average: (dailyExercises.reduce((accumulator, currentValue) => accumulator + currentValue))/dailyExercises.length,
-    success: false,
-    rating: 0,
-    ratingDescription: 'nothing'
-  }
-  result.success = result.average >= result.target
-  result.rating = result.average >= result.target ? 3 : result.average <= 1 ? 1 : 2
-  result.ratingDescription = result.rating === 3 ? 'Good work keep it up' : result.rating === 2 ? 'not too bad but could be better' : 'Pull up your socks'
-  return result
-}
+export const calculateExercise = (
+	dailyExercises: number[],
+	target: number
+): Result => {
+	const result: Result = {
+		target,
+		periodLength: dailyExercises.length,
+		trainingDays: dailyExercises.filter((dailyExercise) => dailyExercise !== 0)
+			.length,
+		average:
+			dailyExercises.reduce(
+				(accumulator, currentValue) => accumulator + currentValue
+			) / dailyExercises.length,
+		success: false,
+		rating: 0,
+		ratingDescription: 'nothing',
+	};
+	result.success = result.average >= result.target;
+	result.rating =
+		result.average >= result.target ? 3 : result.average <= 1 ? 1 : 2;
+	result.ratingDescription =
+		result.rating === 3
+			? 'Good work keep it up'
+			: result.rating === 2
+			? 'not too bad but could be better'
+			: 'Pull up your socks';
+	return result;
+};
 
 try {
-  const { value1, value2 } = parseArguments(process.argv)
-  console.log(calculateExercise(value2, value1))
+	const { dailyExercises, target } = parseArguments(process.argv);
+	console.log(calculateExercise(dailyExercises, target));
 } catch (error: unknown) {
-  let errorMessage: string = 'Something bad happened.'
-  if (error instanceof Error) {
-    errorMessage += ' Error: ' + error.message
-  }
-  console.log(errorMessage);
-
+	let errorMessage: string = 'Something bad happened.';
+	if (error instanceof Error) {
+		errorMessage += ' Error: ' + error.message;
+	}
+	console.log(errorMessage);
 }
