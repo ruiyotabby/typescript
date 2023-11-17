d977e29b21d3eb985f6d85a40766eb406a468861
Ruiyot Abby <ruiyotabby@gmail.com>
2023-11-17 22:50:39 +0300

    Add an api from bmi calcculator

--- index.ts
+++ index.ts
@@ -1,5 +1,22 @@
-import { calculateBmi } from "./bmiCalculator";
-import { calculateExercise } from "./exerciseCalculator";
+import express from 'express';
+import { calculateBmi } from './bmiCalculator';
+const app = express();
+app.use(express.json());
 
-console.log(calculateBmi(172, 52));
-console.log(calculateExercise([3, 0, 2, 4.5, 0, 3, 1], 2))
+app.get('/bmi', (req, res) => {
+	const { height, weight } = req.query;
+
+	if (!height || !weight || isNaN(Number(height)) || isNaN(Number(weight))) {
+		res.status(400).json({ error: 'malformatted parameters' });
+	}
+
+	res.json({
+		height: Number(height),
+		weight: Number(weight),
+		bmi: calculateBmi(Number(height), Number(weight)),
+	});
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
