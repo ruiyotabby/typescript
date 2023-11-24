11ff620e1e5228cdb07fb38e137ea4c7d0d168de
Ruiyot Abby <ruiyotabby@gmail.com>
2023-11-24 23:56:27 +0300

    Add type usage and narrowing

--- courseinfo/src/App.tsx
+++ courseinfo/src/App.tsx
@@ -1,17 +1,62 @@
 const App = () => {
 	const courseName = 'Half Stack application development';
-	const courseParts = [
+	interface CoursePartBase {
+		name: string;
+		exerciseCount: number;
+	}
+
+	interface CourseDescription extends CoursePartBase {
+		description: string;
+	}
+
+	interface CoursePartBasic extends CourseDescription {
+		kind: 'basic';
+	}
+
+	interface CoursePartGroup extends CoursePartBase {
+		groupProjectCount: number;
+		kind: 'group';
+	}
+
+	interface CoursePartBackground extends CourseDescription {
+		backgroundMaterial: string;
+		kind: 'background';
+	}
+
+	type CoursePart = CoursePartBasic | CoursePartGroup | CoursePartBackground;
+
+	const courseParts: CoursePart[] = [
 		{
 			name: 'Fundamentals',
 			exerciseCount: 10,
+			description: 'This is an awesome course part',
+			kind: 'basic',
 		},
 		{
 			name: 'Using props to pass data',
 			exerciseCount: 7,
+			groupProjectCount: 3,
+			kind: 'group',
+		},
+		{
+			name: 'Basics of type Narrowing',
+			exerciseCount: 7,
+			description: 'How to go from unknown to string',
+			kind: 'basic',
 		},
 		{
 			name: 'Deeper type usage',
 			exerciseCount: 14,
+			description: 'Confusing description',
+			backgroundMaterial:
+				'https://type-level-typescript.com/template-literal-types',
+			kind: 'background',
+		},
+		{
+			name: 'TypeScript in frontend',
+			exerciseCount: 10,
+			description: 'a hard part',
+			kind: 'basic',
 		},
 	];
 
@@ -33,17 +78,64 @@ const Header = ({ courseName }: { courseName: string }) => (
 	<h1>{courseName}</h1>
 );
 
-const Content = ({
-	courseParts,
-}: {
-	courseParts: { name: string; exerciseCount: number }[];
-}) =>
-	courseParts.map((coursePart) => (
-		<p>
-			{coursePart.name} {coursePart.exerciseCount}
-		</p>
-	));
+interface CoursePartProp {
+	name: string;
+	exerciseCount: number;
+	kind: 'basic' | 'background' | 'group';
+	description?: string;
+	groupProjectCount?: number;
+	backgroundMaterial?: string;
+}
 
+const Content = ({ courseParts }: { courseParts: CoursePartProp[] }) => {
+	return courseParts.map((coursePart, id) => (
+		<Part key={id} coursePart={coursePart} />
+	));
+};
+
+const Part = ({ coursePart }: { coursePart: CoursePartProp }) => {
+	let course = <></>;
+	switch (coursePart.kind) {
+		case 'basic':
+			course = (
+				<>
+					<em>{coursePart.description}</em>
+				</>
+			);
+			break;
+		case 'group':
+			course = <>project exercises {coursePart.groupProjectCount}</>;
+			break;
+		case 'background':
+			course = (
+				<>
+					<em>{coursePart.description}</em>
+					<div>submit to {coursePart.backgroundMaterial}</div>
+				</>
+			);
+			break;
+		default:
+			(coursePart: never): never => {
+				throw new Error(
+					`Unhandled disciminated union: ,
+					${JSON.stringify(coursePart)}`
+				);
+			};
+			break;
+	}
+
+	return (
+		<div>
+			<div>
+				<strong>
+					{coursePart.name} {coursePart.exerciseCount}
+				</strong>
+			</div>
+			{course}
+			<p></p>
+		</div>
+	);
+};
 const Total = ({ total }: { total: number }) => (
 	<p>Number of exercises {total}</p>
 );
